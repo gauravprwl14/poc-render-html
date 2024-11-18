e04df209a1fa2da404dbd7dd8c98d512c5a50f30
Gaurav Porwal <gauravp@geekyants.com>
2024-11-18 16:27:43 +0530

    feat: enhance initial props handling in App component for improved hydration and error management

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,33 @@ interface AppProps {
 // };
 
 const App: React.FC<AppProps> = (props) => {
+  let data = props.data;
+  if (
+    process.env.NODE_ENV === "production" &&
+    typeof window !== "undefined" &&
+    window.__INITIAL_PROPS__
+  ) {
+    try {
+      let initialData = JSON.parse(window.atob(window.__INITIAL_PROPS__));
+      window.__INITIAL_PROPS__ = undefined;
+
+      // Remove the script tag containing `window.__INITIAL_PROPS__` after hydration
+      delete window.__INITIAL_PROPS__; // Clear the data from `window` object
+      const initialPropsScript = document.getElementById("hydration-script");
+      if (initialPropsScript) {
+        initialPropsScript.remove(); // Remove the script tag from the DOM
+      }
+      data = initialData;
+    } catch (error) {
+      console.error("Error parsing initial props:", error);
+    }
+  }
+
   return (
     <div>
       <h1>{props.title}</h1>
       <p className="text-red-400">{props.content}</p>
-      {props.data && <HTMLRender data={props.data} />}
+      {data && <HTMLRender data={data} />}
     </div>
   );
 };
